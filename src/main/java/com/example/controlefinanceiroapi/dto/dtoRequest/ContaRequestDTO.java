cdec755394fb873777c7c332bdff0e89a8bf8e43
Wiviny <wivinyl@gmail.com>
2026-04-03 17:19:39 -0300

    Melhorando os DTOs

--- src/main/java/com/example/controlefinanceiroapi/dto/dtoRequest/ContaRequestDTO.java
+++ src/main/java/com/example/controlefinanceiroapi/dto/dtoRequest/ContaRequestDTO.java
@@ -1,11 +1,12 @@
 package com.example.controlefinanceiroapi.dto.dtoRequest;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 import java.util.UUID;
 
 public record ContaRequestDTO(@NotBlank(message = "Nome é obrigatório") String nome,
-                              @NotBlank(message = "Saldo é obrigatório")  @PositiveOrZero(message = "O saldo não pode ser negativo") double saldo,
-                              @NotBlank(message = "O id do usuario é obrigatório") UUID usuarioId) {
+                              @NotNull(message = "Saldo é obrigatório")  @PositiveOrZero(message = "O saldo não pode ser negativo") Double saldo,
+                              @NotNull(message = "O id do usuario é obrigatório") UUID usuarioId) {
 }
--- src/main/java/com/example/controlefinanceiroapi/dto/dtoRequest/TransacaoRequestDTO.java
+++ src/main/java/com/example/controlefinanceiroapi/dto/dtoRequest/TransacaoRequestDTO.java
@@ -2,14 +2,14 @@ package com.example.controlefinanceiroapi.dto.dtoRequest;
 
 import com.example.controlefinanceiroapi.enumeracao.TipoEnum;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.UUID;
 
 public record TransacaoRequestDTO(@NotBlank(message = "Descrição é obrigatório") String descricao,
                                   @NotBlank(message = "Valor é obrigatório") @PositiveOrZero(message = "O valor tem que ser positivo") double valor,
-                                  @NotBlank(message = "Tipo é obrigatório") TipoEnum tipo,
-                                  @NotBlank(message = "O id da conta é obrigatório") UUID contaId,
-                                  @NotBlank(message = "O id da categoria é obrigatório") UUID categoriaId) {
+                                  @NotNull(message = "Tipo é obrigatório") TipoEnum tipo,
+                                  @NotNull(message = "O id da conta é obrigatório") UUID contaId,
+                                  @NotNull(message = "O id da categoria é obrigatório") UUID categoriaId) {
 }
