eb19951c42fe973ecff546584f5fbae42adf8e2f
Wiviny <wivinyl@gmail.com>
2026-04-03 17:22:46 -0300

    .

--- src/main/java/com/example/controlefinanceiroapi/dto/dtoRequest/TransacaoRequestDTO.java
+++ src/main/java/com/example/controlefinanceiroapi/dto/dtoRequest/TransacaoRequestDTO.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.PositiveOrZero;
 import java.util.UUID;
 
 public record TransacaoRequestDTO(@NotBlank(message = "Descrição é obrigatório") String descricao,
-                                  @NotBlank(message = "Valor é obrigatório") @PositiveOrZero(message = "O valor tem que ser positivo") double valor,
+                                  @NotNull(message = "Valor é obrigatório") @PositiveOrZero(message = "O valor tem que ser positivo") Double valor,
                                   @NotNull(message = "Tipo é obrigatório") TipoEnum tipo,
                                   @NotNull(message = "O id da conta é obrigatório") UUID contaId,
                                   @NotNull(message = "O id da categoria é obrigatório") UUID categoriaId) {
